c37b645935d26bb53c396c2afe67e089921c0b66
Muhammad-Hidayah <2202150@sit.singaporetech.edu.sg>
2026-03-19 06:47:01 +0800

    feat(notifications): register NotificationsModule and ScheduleModule in AppModule

---
+++ apps/api/src/modules/notifications/notifications.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { NotificationsController } from './notifications.controller';
+import { NotificationsService } from './notifications.service';
+import { ComplianceScannerJob } from './jobs/compliance-scanner.job';
+import { MaintenanceReminderJob } from './jobs/maintenance-reminder.job';
+import { WorkshopFreshnessJob } from './jobs/workshop-freshness.job';
+
+@Module({
+  controllers: [NotificationsController],
+  providers: [
+    NotificationsService,
+    ComplianceScannerJob,
+    MaintenanceReminderJob,
+    WorkshopFreshnessJob,
+  ],
+  exports: [NotificationsService],
+})
+export class NotificationsModule {}
